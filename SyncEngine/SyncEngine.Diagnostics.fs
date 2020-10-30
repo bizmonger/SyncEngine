487cc89c4caaab53c1df107d031e0ae5b551949e
Bizmonger <scott.nimrod@bizmonger.net>
2020-10-29 20:24:19 -0400

    Added skeleton functions for diagnostics

--- SyncEngine/SyncEngine.Diagnostics.fs
+++ SyncEngine/SyncEngine.Diagnostics.fs
@@ -1,8 +1,10 @@
 ﻿namespace SyncEngine
 
-open SyncEngine.Language
-open SyncEngine.Operations
+open SyncEngine.Diagnostics.Operations
 
 module Diagnostics =
 
-    let foo() = ()
+    let log             : Log<'submission,'response> = fun _ -> async.Return <| Error "not implemented"
+    let dataSyncItemLog : DataSyncItemLog            = fun _ -> async.Return <| Error "not implemented"
+    let report          : Report                     = fun _ -> async.Return <| Error "not implemented"
+    let flush           : Flush                      = fun _ -> async.Return <| Error "not implemented"
